2d3a72d318ac24c4cf42de8836574b93f9d53b28
MariaRaya <94574813+MariaRaya@users.noreply.github.com>
2021-12-02 13:57:05 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,23 @@
 var trex, trex_running, edges;
 var groundImage, ground, groundInvisivel;
-var nuvem, nuvemImagem;
+var nuvem, nuvemImagem, nuvemGrupo;
+var obstaculo, obstaculo1, obstaculo2, obstaculo3, obstaculo4, obstaculo5, obstaculo6, obstaculoGrupo;
+var aleatorio, pontuacao;
+
+var FIM = 0;
+var PLAY = 1;
+var estadoJogo = PLAY;
 
 function preload(){  //Carrega a imagem
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   groundImage = loadImage("ground2.png");
   nuvemImagem = loadImage("cloud.png");
+  obstaculo1 = loadImage("obstacle1.png");
+  obstaculo2 = loadImage("obstacle2.png");
+  obstaculo3 = loadImage("obstacle3.png");
+  obstaculo4 = loadImage("obstacle4.png");
+  obstaculo5 = loadImage("obstacle5.png");
+  obstaculo6 = loadImage("obstacle6.png");
 }
 
 function setup(){
@@ -28,6 +40,12 @@ function setup(){
   //Chão invisível
   groundInvisivel = createSprite(200,190,400,10);
   groundInvisivel.visible = false;
+
+  //Pontuação
+  pontuacao = 0;
+
+  nuvemGrupo = new Group();
+  obstaculoGrupo = new Group();
 }
 
 
@@ -35,20 +53,40 @@ function draw(){
   //definir a cor do plano de fundo 
   background("white");
 
-  //movimento do chão
-  ground.velocityX = -2;
-  if (ground.x < 0){
-    ground.x = ground.width / 2;  // Faz a repetição do chão 
-  }
-  
-  //pular quando tecla de espaço for pressionada
-  if(keyDown("space") && trex.y > 100){
+  text("Pontuação: " + pontuacao, 500, 40);
+ 
+  if (estadoJogo == PLAY){
+    //movimento do chão
+    ground.velocityX = -2;
+    pontuacao = pontuacao + Math.round(frameCount/60);
+
+    if (ground.x < 0){
+      ground.x = ground.width / 2;  // Faz a repetição do chão 
+    }
+
+     //pular quando tecla de espaço for pressionada
+    if(keyDown("space") && trex.y > 100){
     trex.velocityY = -10;
+    }
+
+    trex.velocityY = trex.velocityY + 0.5; // Faz com que o T-Rex retorne ao chão
+
+    // Criar nuvens
+    criarNuvens ();
+
+    //Criar obstáculos 
+    criarObstaculos ();
+
+    if (obstaculoGrupo.isTouching(trex)){
+      estadoJogo = FIM;
+    }
+
+  } else if (estadoJogo == FIM) {
+    ground.velocityX = 0;
+    obstaculoGrupo.setVelocityXEach (0);
+    nuvemGrupo.setVelocityXEach (0);
   }
-  
-  trex.velocityY = trex.velocityY + 0.5; // Faz com que o T-Rex retorne ao chão
-  // Criar nuvens
-  criarNuvens ();
+
   console.log(frameCount);
  //impedir que o trex caia
   trex.collide(groundInvisivel);
@@ -63,5 +101,52 @@ function criarNuvens(){
     nuvem.addImage(nuvemImagem);
     nuvem.y = random(30,100);
     nuvem.scale = 0.6;
+
+    nuvem.lifetime = 220;  // vida da nuvem
+
+    // Ajuste de profundidade
+
+    nuvem.depth = trex.depth;
+    trex.depth = trex.depth + 1; 
+
+    nuvemGrupo.add(nuvem);
+  }
+}
+
+function criarObstaculos (){
+  if (frameCount % 50 == 0){
+    obstaculo = createSprite(400,165,10,40);
+    obstaculo.velocityX = -5;
+
+    aleatorio = Math.round(random(1,6));
+
+      switch(aleatorio){
+        case 1: obstaculo.addImage(obstaculo1);
+        break;
+
+        case 2: obstaculo.addImage(obstaculo2);
+        break;
+
+        case 3: obstaculo.addImage(obstaculo3);
+        break;
+
+        case 4: obstaculo.addImage(obstaculo4);
+        break;
+
+        case 5: obstaculo.addImage(obstaculo5);
+        break;
+
+        case 6: obstaculo.addImage(obstaculo6);
+        break;
+
+        default: break;
+     }
+
+      // Escala + Tempo de Vida
+
+      obstaculo.scale = 0.45;
+      obstaculo.lifetime = 220;
+
+      obstaculoGrupo.add(obstaculo);
   }
 }
